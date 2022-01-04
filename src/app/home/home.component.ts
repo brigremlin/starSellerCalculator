17a9d359fea0d006689174e410bb6e46b4e23671
Brianna Grimsley <brigrimsley@gmail.com>
2022-01-03 22:40:12 -0600

    bg: calculations
    Updated calculations to reflect accurate output

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,15 @@ export class HomeComponent implements OnInit {
   messageNumber: number;
   messagePercentage: number;
   respondedMessages: number;
-  neededMessaged: number;
+  neededMessages: number;
+  shipmentNumber: number;
+  shipmentPercentage: number;
+  neededShipments: number;
+  shipped: number;
+  ratingNumber: number;
+  ratingPercentage: number;
+  ratings: number;
+  fiveStar: number;
 
   constructor() {}
 
@@ -18,18 +26,107 @@ export class HomeComponent implements OnInit {
   calculateMessage(messageNumber: number, messagePercentage: number) {
     messagePercentage = this.messagePercentage;
     this.respondedMessages = Math.round(messageNumber * messagePercentage);
-    console.log(this.respondedMessages);
-    console.log(this.messagePercentage);
-    while (this.messagePercentage < 0.95) {
+    while (messagePercentage <= 0.945) {
       this.messageNumber++;
       this.respondedMessages++;
-      this.messagePercentage = this.respondedMessages / this.messageNumber;
-      var num: number = Math.round(this.messagePercentage*100);
-      console.log(num);
-      if (num >= 95) {
-        console.log('I am' + num);
-        this.neededMessaged = this.messageNumber - this.respondedMessages;
+      messagePercentage = this.respondedMessages / this.messageNumber;
+      var num = new Number(messagePercentage);
+      console.log(num.toPrecision(2));
+      console.log(this.messageNumber);
+      console.log(this.respondedMessages);
+      if (num >= 0.945) {
+        this.neededMessages = this.messageNumber - messageNumber;
+        console.log('I am' + this.neededMessages);
+        return this.neededMessages;
       }
     }
+  }
+
+  calculateShipments(shipmentNumber: number, shipmentPercentage: number) {
+    shipmentPercentage = this.shipmentPercentage;
+    this.shipped = Math.round(shipmentNumber * shipmentPercentage);
+    while (shipmentPercentage <= 0.945) {
+      this.shipmentNumber++;
+      this.shipped++;
+      shipmentPercentage = this.shipped / this.shipmentNumber;
+      var num = new Number(shipmentPercentage);
+      console.log(num.toPrecision(2));
+      console.log(this.shipmentNumber);
+      console.log(this.shipped);
+      if (num >= 0.945) {
+        this.neededShipments = this.shipmentNumber - shipmentNumber;
+        console.log('I am' + this.neededShipments);
+        return this.neededShipments;
+      }
+    }
+  }
+
+  calculateRating(ratingNumber: number, fiveStar: number) {
+    var fiveStarPercentage = fiveStar / ratingNumber;
+    console.log(fiveStarPercentage);
+    while (fiveStarPercentage <= 0.945) {
+      this.ratingNumber++;
+      this.fiveStar++;
+      fiveStarPercentage = this.fiveStar / this.ratingNumber;
+      var num = new Number(fiveStarPercentage);
+      console.log(num.toPrecision(2));
+      if (num >= 0.945) {
+        this.ratings = this.fiveStar - fiveStar;
+        return this.ratings;
+      }
+    }
+  }
+
+  calculateStarSeller(
+    messageNumber: number,
+    messagePercentage: number,
+    shipmentNumber: number,
+    shipmentPercentage: number,
+    ratingNumber: number,
+    fiveStar: number
+  ) {
+    var fiveStarPercentage = fiveStar / ratingNumber;
+    console.log(fiveStarPercentage);
+    while (fiveStarPercentage <= 0.945) {
+      this.ratingNumber++;
+      this.fiveStar++;
+      fiveStarPercentage = this.fiveStar / this.ratingNumber;
+      var num = new Number(fiveStarPercentage);
+      console.log(num.toPrecision(2));
+      if (num >= 0.945) {
+        this.ratings = this.fiveStar - fiveStar;
+      }
+    }
+    shipmentPercentage = this.shipmentPercentage;
+    this.shipped = Math.round(shipmentNumber * shipmentPercentage);
+    while (shipmentPercentage <= 0.945) {
+      this.shipmentNumber++;
+      this.shipped++;
+      shipmentPercentage = this.shipped / this.shipmentNumber;
+      var num = new Number(shipmentPercentage);
+      console.log(num.toPrecision(2));
+      console.log(this.shipmentNumber);
+      console.log(this.shipped);
+      if (num >= 0.945) {
+        this.neededShipments = this.shipmentNumber - shipmentNumber;
+        console.log('I am' + this.neededShipments);
+      }
+    }
+    messagePercentage = this.messagePercentage;
+    this.respondedMessages = Math.round(messageNumber * messagePercentage);
+    while (messagePercentage <= 0.945) {
+      this.messageNumber++;
+      this.respondedMessages++;
+      messagePercentage = this.respondedMessages / this.messageNumber;
+      var num = new Number(messagePercentage);
+      console.log(num.toPrecision(2));
+      console.log(this.messageNumber);
+      console.log(this.respondedMessages);
+      if (num >= 0.945) {
+        this.neededMessages = this.messageNumber - messageNumber;
+        console.log('I am' + this.neededMessages);
+      }
+    }
+    return this.neededMessages,this.ratings, this.neededShipments;
   }
 }
